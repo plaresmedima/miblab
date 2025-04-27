3fd0e1369760b52d514121833f82a2e1d0391812
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2025-04-27 17:19:26 +0100

    Add website analytics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -135,7 +135,10 @@ html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     "github_url": "https://github.com/openmiblab/miblab",
     "collapse_navigation": True,
-    }
+    "analytics": {
+        "google_analytics_id": "G-62S7WYNRKR",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory. They are copied after the builtin static files, so a file named "default.css" will overwrite the builtin "default.css"
 html_static_path = ['_static']
